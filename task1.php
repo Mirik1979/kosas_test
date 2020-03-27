5d4ba93d5e12e856649d412879a54e9f09b7c78e
Miros <lya-job@yandex.ru>
2020-03-27 20:14:03 +0300

    Формы ввода данных

--- task1.php
+++ task1.php
@@ -1,1 +1,18 @@
 <?php
+//$result = "";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    //print_r($_POST);
+    $resulttext = "Результат:";
+    $result = $_POST['values'];
+}
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<form action="" method="post">
+    <p><b>Введите последовательность чисел через запятую:</b><br>
+        <input name="values" type="text" size="40">
+    </p>
+    <input name="operation" type="submit" value="Сопоставить наибольшее число"/>
+</form>
+<? echo $resulttext.$result; ?>
+
--- task2.php
+++ task2.php
@@ -1,1 +1,17 @@
 <?php
+//$result = "";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    //print_r($_POST);
+    $resulttext = "Результат:";
+    $result = $_POST['values'];
+}
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<form action="" method="post">
+    <p><b>Введите последовательность чисел через запятую:</b><br>
+        <input name="values" type="text" size="40">
+    </p>
+    <input name="operation" type="submit" value="Вывести список диапазонов"/>
+</form>
+<? echo $resulttext.$result; ?>
