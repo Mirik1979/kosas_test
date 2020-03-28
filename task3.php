3de39dbdd55900967a7f54d8c620f9bd36a676e7
Miros <lya-job@yandex.ru>
2020-03-28 20:16:49 +0300

    третье задание

--- task3.php
+++ task3.php
@@ -4,6 +4,27 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     //print_r($_POST);
     $resulttext = "Результат:";
     $result = $_POST['values'];
+    $resulttext = "Результат:";
+    $formval = preg_replace('/\s+/', '', $_POST['values']);
+    $result = longestLength($formval);
+}
+
+function longestLength($string)
+{
+    $len = strlen($string);
+    for($i = $len; $i > 1; $i--) {
+        $subStrLength = $i;
+        $offset = 0;
+        do
+        {
+            $subStr = substr($string, $offset, $subStrLength);
+            if(count(array_unique(str_split($subStr))) == $subStrLength) {
+                return $subStrLength;
+            }
+            $offset++;
+        } while($offset + $subStrLength <= $len);
+    }
+    return $i;
 }
 ?>
     <!DOCTYPE html>
@@ -12,6 +33,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         <p><b>Введите последовательность чисел через запятую:</b><br>
             <input name="values" type="text" size="40">
         </p>
-        <input name="operation" type="submit" value="Вывести самую длинную подстроку"/>
+        <input name="operation" type="submit" value="Вывести длину макс последовательности"/>
     </form>
 <? echo $resulttext.$result; ?>
