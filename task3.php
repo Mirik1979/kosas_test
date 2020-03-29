03b2f6732fc8bec364e67ceadf595f9220d1412e
Miros <lya-job@yandex.ru>
2020-03-29 14:35:47 +0300

    третье задание (второй вариант функции)

--- task3.php
+++ task3.php
@@ -7,8 +7,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $resulttext = "Результат:";
     $formval = preg_replace('/\s+/', '', $_POST['values']);
     $result = longestLength($formval);
-}
+    $result2 = "(по второму варианту функции:".longestLength2($formval).")";
 
+}
+// первый вариант
 function longestLength($string)
 {
     $len = strlen($string);
@@ -26,6 +28,30 @@ function longestLength($string)
     }
     return $i;
 }
+// второй вариант
+function longestLength2($string)
+{
+    $arr = array();
+    for ($i = 0; $i < mb_strlen($string); $i++) {
+        $arr[$i] = array();
+        for ($m = $i; $m < mb_strlen($string); $m++) {
+            $char = mb_substr($string, $m, 1);
+            if (!in_array($char ,$arr[$i])) {
+                array_push($arr[$i], $char);
+            } else {
+                break;
+            }
+        }
+    }
+    $mcount = 0;
+    foreach ($arr as $arrval) {
+        $count = count($arrval);
+        if($count >= $mcount) {
+            $mcount = $count;
+        }
+    }
+    return $mcount;
+}
 ?>
     <!DOCTYPE html>
     <html lang="ru">
@@ -35,4 +61,4 @@ function longestLength($string)
         </p>
         <input name="operation" type="submit" value="Вывести длину макс последовательности"/>
     </form>
-<? echo $resulttext.$result; ?>
+<? echo $resulttext.$result.$result2; ?>
