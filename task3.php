521045b5b896e0b5d934d2e031e8ab3f82f3c9c8
Miros <lya-job@yandex.ru>
2020-03-31 19:41:36 +0300

    исправления по итогам обратной связи

--- task3.php
+++ task3.php
@@ -1,18 +1,19 @@
 <?php
-//$result = "";
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    //print_r($_POST);
-    $resulttext = "Результат:";
+    // преобразовываем данные из формы в массив, убирая пробелы
     $result = $_POST['values'];
     $resulttext = "Результат:";
     $formval = preg_replace('/\s+/', '', $_POST['values']);
+    // считаем длину первым способом
     $result = longestLength($formval);
+    // считаем длину вторым способом
     $result2 = "(по второму варианту функции:".longestLength2($formval).")";
 
 }
-// первый вариант
+// первый способ
 function longestLength($string)
 {
+    // считаем общую длину строки
     $len = strlen($string);
     for($i = $len; $i > 1; $i--) {
         $subStrLength = $i;
@@ -20,32 +21,36 @@ function longestLength($string)
         do
         {
             $subStr = substr($string, $offset, $subStrLength);
+            // возвращаем максимальную длину при выполнении заданных условий
             if(count(array_unique(str_split($subStr))) == $subStrLength) {
                 return $subStrLength;
             }
             $offset++;
         } while($offset + $subStrLength <= $len);
     }
+    // возвращаем максимальную длину если проверочные условия не были выполнены
     return $i;
 }
 // второй вариант
 function longestLength2($string)
 {
+    // создаем пустой массив для сборки различных комбинаций символов
     $arr = array();
+    $mcount = 0;
     for ($i = 0; $i < mb_strlen($string); $i++) {
         $arr[$i] = array();
+        // перебираем подстроки, наполняя массив с комбинациями символов
         for ($m = $i; $m < mb_strlen($string); $m++) {
             $char = mb_substr($string, $m, 1);
             if (!in_array($char ,$arr[$i])) {
                 array_push($arr[$i], $char);
             } else {
+                // останавливаемся, если символ неуникальный
                 break;
             }
         }
-    }
-    $mcount = 0;
-    foreach ($arr as $arrval) {
-        $count = count($arrval);
+        // подсчитываем наибольшее значение массива
+        $count = count($arr[$i]);
         if($count >= $mcount) {
             $mcount = $count;
         }
